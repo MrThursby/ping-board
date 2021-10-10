c40696c18330f3910b671462074096a43c44b0d0
MrThursby <smykoil@ya.ru>
2021-10-10 20:01:17 +0300

    Create login page, layouts; bootswatch install

---
+++ layouts/sign.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex vh-100 align-items-center justify-content-center">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sign"
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-form-group
+      label="Логин"
+    >
+      <b-form-input v-model="form.login" />
+    </b-form-group>
+    <b-form-group
+      label="Пароль"
+    >
+      <b-form-input v-model="form.password" />
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  layout: "sign",
+  data: () => ({
+    form: {
+      login: '',
+      password: '',
+    }
+  })
+}
+</script>
